3590a92f1b9f70b936c76a60cd731ccc6a829f25
chanlu <chanlu@149-160-200-119.dhcp-bl.indiana.edu>
2020-10-27 19:56:02 -0400

    addfile

---
+++ practicals/segmenter.py
@@ -0,0 +1,12 @@
+import sys
+text = sys.stdin.read()
+for c in text:
+        if c == '.':
+                text = text.replace('. ','.\n')
+        if c == ')':
+                text = text.replace('.) ','.\n')
+        if c == '?':
+                text = text.replace('? ','.\n')
+        if c == '!':
+                text = text.replace('! ','.\n')
+print(text)
