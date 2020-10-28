77babc251654938822db9ee37a9dd97f42a12e0f
mchanitlu <56201906+mchanitlu@users.noreply.github.com>
2020-10-27 21:35:48 -0400

    Update transcriber.py

--- practicals/transcriber.py
+++ practicals/transcriber.py
@@ -1,56 +1,27 @@
 import sys
-import itertools
-text = sys.stdin.read()	
-z = text.splitlines()
-#print(z)  
-#for i in range(len(z)):
-#	print("%d\t%s\n"%(i+1,z[i]))
-def newlist(z):
-	res = []
-	for el in z:
-		sub = el.split("!@#$%^&*")
-		res.append(sub)
-	res = [ele for ele in res if ele != [' ']] 
-	res = [ele for ele in res if ele != ['']]  
-	return(res) 
-w = newlist(z)
-#print("w")
-#print(w)
-#for i in range(len(w)):
-#	print("%d\t%s"%(i+1,w[i]))
-a = "ID\tFROM\tLEMMA\tUPOS\tXPOS\tFEATS\tHEAD\tDEPREL\tDEPS\tMISC"
-flat_list = []
-for sublist in w:
-	for item in sublist:
-		punc = item.replace('"','\n"\n')
-		punc = punc.replace(') ','\n)\n')
-		punc = punc.replace(' (','\n(\n')
-		punc = punc.replace(',','\n,\n')
-		punc = punc.replace('.','\n.\n')
-		ok = punc.split()
-		flat_list.append(ok)
-flatten = lambda w: [item for sublist in w for item in sublist]
-a = "ID\tFROM\tLEMMA\tUPOS\tXPOS\tFEATS\tHEAD\tDEPREL\tDEPS\tMISC"
-print(a)
-for i in range(len(flat_list)):
-	print('# sent_id = %s' % (i+1))
-	nano = ' '.join(flat_list[i])
-	print("# text = " + str(nano))
-	flat_list[i] = list(filter(lambda item: item.strip(), flat_list[i]))
-	for c in range(len(flat_list[i])):
-		u = flat_list[i]
-		if u[c] == ',':
-			print("%d\t%s\t_\t_\t_\t_\t_\t_\t_"%(c+1,u[c]))
-		elif u[c] == '-':
-                        print("%d\t%s\t_\t_\t_\t_\t_\t_\t_"%(c+1,u[c]))
-		elif u[c] == '"':
-                        print("%d\t%s\t_\t_\t_\t_\t_\t_\t_"%(c+1,u[c]))
-		elif u[c] == ')':
-                        print("%d\t%s\t_\t_\t_\t_\t_\t_\t_"%(c+1,u[c]))
-		elif u[c] == '(':
-                        print("%d\t%s\t_\t_\t_\t_\t_\t_\t_"%(c+1,u[c]))
-		elif u[c] == '.':
-                        print("%d\t%s\t_\t_\t_\t_\t_\t_\t_"%(c+1,u[c]))
+def ss(word,ipa):
+	ret = ""
+	i = 0 
+	while i < len(word):
+		if i+1<(len(word)):
+			c = word[i: i+2]
+			if c in ipa:
+				ret = ret + ipa[c]
+				i = i+2
+				continue
+		c = word[i]	
+		if c in ipa:
+			ret = ret + ipa[c]
 		else:
-			print("%d\t%s\t_\t_\t_\t_\t_\t_\t_\t_"%(c+1,u[c]))
+			ret = ret + c
+		i = i + 1
+		
+	return ret
+ 
+if __name__ == "__main__":
+	ipa = {}
+	dtemp = {'a': 'a', 'b': 'b', 'v': 'b', 'c': 'k','que':'kue','qui':'kui', 'ch' : 'tʃ','qua':'kwa','quo'='kwo','küe'='kwe','küi'='kwe','d'='d','e'='e','f'='f','g'='g','gua'='gwa','guo'='gwo','güe'='gwe','güi'='gwi','i'='i','h'='h','i'='i','l'='l','ʎ'='ll','m'='m','n'='n','ɲ'='ny','o'='o','p'='p','r'='ɾ','rr'='r',' r'='r', 's'='s','t'='t','u'='u','ix'='jʃ','y'='j','za='θ','zo='θ','zu'='θ','ce'='θ','ci'='θ'}
+	for k,v in dtemp.items():
+		ipa[k] = v
+	for line in sys.stdin.readlines():
 		
