825cf9900d160173f92ae9191b057fbda869997e
mchanitlu <56201906+mchanitlu@users.noreply.github.com>
2020-10-27 21:03:24 -0400

    Update trainer.py

--- practicals/trainer.py
+++ practicals/trainer.py
@@ -20,9 +20,6 @@ def ss(word,ipa):
  
 if __name__ == "__main__":
 	ipa = {}
-	#temp = ['i', 'u', 'e', 'o', 'a', 'm', 'n', 'ɲ', 'p', 't', 'k', 'b', 'd', 'g', 'f', 'θ', 's', 'ʃ', 'j', 'w', 'l','ʎ', 'ɾ', 'r']
-	#for c in range(len(temp)):
-	#	ipa[chr(ord('a')) + i] = temp[i]
 	dtemp = {'a': 'a', 'b': 'b', 'v': 'b', 'c': 'k','que':'kue','qui':'kui', 'ch' : 'tʃ','qua':'kwa','quo'='kwo','küe'='kwe','küi'='kwe','d'='d','e'='e','f'='f','g'='g','gua'='gwa','guo'='gwo','güe'='gwe','güi'='gwi','i'='i','h'='h','i'='i','l'='l','ʎ'='ll','m'='m','n'='n','ɲ'='ny','o'='o','p'='p','r'='ɾ','rr'='r',' r'='r', 's'='s','t'='t','u'='u','ix'='jʃ','y'='j','za='θ','zo='θ','zu'='θ','ce'='θ','ci'='θ'}
 	for k,v in dtemp.items():
 		ipa[k] = v
