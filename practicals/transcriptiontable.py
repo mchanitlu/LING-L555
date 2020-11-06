687e14e034397afb79e8239e784da8007b714362
mchanitlu <56201906+mchanitlu@users.noreply.github.com>
2020-11-05 20:18:52 -0500

    Add files via upload

---
+++ practicals/transcriptiontable.py
@@ -0,0 +1,5 @@
+import sys
+dtemp = {'v':'b','que':'kue','qui':'kui','ch': 'tʃ','qu':'k','qü':'kw','gua':'gwa','güi':'gwi','güe':'gwi','ll':'ʎ','ny':'ɲ','rr':'rr','r':'ɾ','ix':'jʃ','y': 'j', 'za': 'θa', 'zo': 'θo', 'zu': 'θu', 'ce': 'θe', 'ci': 'θi'}
+for k,v in dtemp.items():
+	print(k,"\t",v)
+
